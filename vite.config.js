f8be13f733a7c193957ea59db54ca5d2fa9ffe94
Valentin <valentin.peupion@epitech.eu>
2026-05-27 10:46:56 +0200

    push pour mettre en ligne

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/B-POR-200-NCY-1-1-portfolio-12/'
+  base: '/portfolioValentin/'
 })
